c0d71f228db133e6c92d23b7874fef6878096b56
DraDN <dra.dragos.dd@gmail.com>
2026-05-16 01:03:18 +0300

    Added Clerk authenticator

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,49 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 // import './App.css'
 
-import { useEffect } from 'react'
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton, useUser } from '@clerk/clerk-react'
+
+
+const API = import.meta.env.VITE_API_URL;
 
 function App() {
-  // const [count, setCount] = useState(0)
   const [status, setStatus] = useState('ok')
+  const [decks, setDecks] = useState([])
+  const { user } = useUser();
 
   useEffect(() => {
-    fetch('http://api.docker:3000/api/health')
+    fetch(`${API}/api/health`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data)
         setStatus(data.status);
       })
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    fetch(`${API}/api/decks?user_id=${user.id}`)
+      .then((res) => res.json())
+      .then(setDecks);
+  }, [user.id]);
 
   return (
     <>
+      <header>
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+
       <h1>{status}</h1>
+      <h2> Test decks: </h2>
+      {decks.map((deck) => (
+        <p key={deck.id}>{deck.name}</p>
+      ))}
     </>
   )
 }
