03114d57a19d5f543fe75b0c3b13626dac38be07
DraDN <dra.dragos.dd@gmail.com>
2026-05-24 22:39:05 +0300

    Fixed dungeon card to being responsive

--- frontend/src/components/DungeonCard.jsx
+++ frontend/src/components/DungeonCard.jsx
@@ -13,9 +13,9 @@ export default function DungeonCard({ dungeon, onDelete, onEdit, onPlay }) {
     return (
         <>
             <div className="bg-dungeon-dark-500 border-dungeon-yellow border text-2xl font-bold flex flex-col justify-start m-5"> 
-                <div className="flex md:flex-row flex-col gap-3 items-center justify-between p-5">
+                <div className="flex md:flex-row flex-col flex-wrap gap-3 items-center justify-between p-5">
                     <span className="font-bold text-3xl">{dungeon.name}</span>
-                    <div className="flex flex-row gap-2 items-center *:p-2 *:rounded-xl *:hover:bg-dungeon-yellow *:transition-colors">
+                    <div className="flex flex-row flex-wrap gap-2 items-center *:p-2 *:rounded-xl *:hover:bg-dungeon-yellow *:transition-colors">
                         <button className='bg-dungeon-red-900' onClick={() => onDelete(dungeon.id)}>Delete</button>
                         <button className="bg-dungeon-purple" onClick={() => onEdit(dungeon.id, dungeon.name)}>Edit</button>
                         <button className="bg-dungeon-green-700" onClick={() => onPlay(dungeon)}>Play</button>
