e9d2e65ab1cee4447806f96bee0c81046496123c
Dragan Dragos Nicolae <dra.dragos.dd@gmail.com>
2026-08-03 22:41:44 +0300

    Added body and query size limiting

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,8 +27,8 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 const rateLimiter = require('./middleware/rateLimiter');
 app.use(rateLimiter);
