c1d8ba47c2834aa132e684ebe1c2aef65826067d
DraDN <dra.dragos.dd@gmail.com>
2026-08-19 15:57:19 +0300

    fix(xp): revised xp and level calculation to be proper with intended level progression

--- frontend/src/utils/xp_utils.js
+++ frontend/src/utils/xp_utils.js
@@ -1,16 +1,22 @@
-const calculateLevelXP = (level) => {
-    return (level - 1) * 100;
+const calculateLevelUpXP = (start_level) => {
+    return (start_level + 1) * 100;
+}
+
+const calculateTotalLevelXP = (level) => {
+    // arithmetic series sum, changed to account for a_n = (n-1)*b aka. ( n = level + 1 )
+    return (level * (level + 1) * 100) / 2;
 }
 
 const calculateNextLevelProgression = (xp, level) => {
-    const xp_level = calculateLevelXP(level);
+    const xp_level = calculateTotalLevelXP(level);
     const xp_diff = xp - xp_level;
-    const level_diff = calculateLevelXP(level + 1) - xp_level;
+    const level_diff = calculateTotalLevelXP(level + 1) - xp_level;
 
     return xp_diff / level_diff * 100; // mult by 100 to get a percentage
 }
 
 export {
-    calculateLevelXP,
+    calculateLevelUpXP,
+    calculateTotalLevelXP,
     calculateNextLevelProgression
 }
