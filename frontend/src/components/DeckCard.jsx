71ea80ef2599c2827341e42850ffb86c70333b78
DraDN <dra.dragos.dd@gmail.com>
2026-08-24 02:55:22 +0300

    fix: updated deck card ui for not having an xp bar anymore

--- frontend/src/components/DeckCard.jsx
+++ frontend/src/components/DeckCard.jsx
@@ -1,12 +1,8 @@
 export default function DeckCard({ deck, onSelect, onRename, onDelete }) {
     return (
-        <div className="bg-dungeon-dark-500 border border-dungeon-yellow text-2xl font-bold flex items-center justify-between m-5 gap-4 p-4">
-            <div className="w-full space-y-2">
-                <div className='flex flex-row gap-2 items-end grow'>
-                    <h1>{deck.name}</h1>
-                </div>
-            </div>
-            <div className='*:p-2 *:rounded-xl *:hover:bg-dungeon-yellow *:transition-colors flex md:flex-row flex-col gap-2'>
+        <div className="bg-dungeon-dark-500 border border-dungeon-yellow text-2xl font-bold flex md:flex-row flex-col items-center justify-between m-5 gap-4 p-4">
+            <h1>{deck.name}</h1>
+            <div className='*:p-2 *:rounded-xl *:hover:bg-dungeon-yellow *:transition-colors flex flex-row gap-2'>
                 <button className='bg-dungeon-red-900' onClick={() => {onDelete(deck.id)}}>Delete</button>
                 <button className='bg-dungeon-purple' onClick={() => {onRename(deck.id, deck.name)}}>Rename</button>
                 <button className='bg-dungeon-green-700' onClick={() => {onSelect(deck)}}>Select</button>
